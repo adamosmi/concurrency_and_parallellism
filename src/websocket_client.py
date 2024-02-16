e642b054cbb9ec7f5b99c8bf39a5a546597e42a6
Andrew Damon-Smith <andrewdamonsmith@gmail.com>
2024-02-16 18:04:38 -0500

    Update SERVER_ADDRESS

--- src/websocket_client.py
+++ src/websocket_client.py
@@ -1,8 +1,11 @@
+import os
 from websockets.sync.client import connect
+
+SERVER_ADDRESS = os.getenv("SERVER_ADDRESS")
 
 
 def hello():
-    with connect("ws://localhost:8765") as websocket:
+    with connect(f"ws://{SERVER_ADDRESS}") as websocket:
         websocket.send("Hello world!")
         message = websocket.recv()
         print(f"Received: {message}")
