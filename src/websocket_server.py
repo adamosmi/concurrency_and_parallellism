0c2f09702ac9353a3e96b17abec1154efdba56b7
Andrew Damon-Smith <andrewdamonsmith@gmail.com>
2024-02-19 20:47:39 -0500

    Add server error handling

--- src/websocket_server.py
+++ src/websocket_server.py
@@ -14,11 +14,16 @@ async def broadcast(message):
 async def handler(websocket):
     # add the connected client
     connected.add(websocket)
-    # listen for messages from websocket
-    async for message in websocket:
-        print(f"Message recieved:\n{message}")
-        # broadcast any message received to
-        await broadcast(message)
+    try:
+        # listen for messages from websocket
+        async for message in websocket:
+            print(f"Message recieved:\n{message}")
+            # broadcast any message received to
+            await broadcast(message)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        connected.remove(websocket)
 
 
 # main
